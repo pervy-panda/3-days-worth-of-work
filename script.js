23d01658bf469009964d366683759e1c83b62cc8
tunamayo <102719424+pervy-panda@users.noreply.github.com>
2024-07-17 18:43:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let currentMessage = null;
+
+function showMessage(messageId) {
+    currentMessage = messageId;
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById(messageId).style.display = 'block';
+}
+
+function closeMessage() {
+    document.getElementById('overlay').style.display = 'none';
+    if (currentMessage) {
+        document.getElementById(currentMessage).style.display = 'none';
+        currentMessage = null;
+    }
+}
+
+
+function openlink(url) {
+    window.open(url, '_blank');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('background-music');
+
+    function playAudio() {
+        audio.play().then(() => {
+            console.log("Audio is playing");
+        }).catch(error => {
+            console.log("Error playing audio: ", error);
+        });
+        document.removeEventListener('click', playAudio);
+        document.removeEventListener('touchstart', playAudio);
+    }
+
+    document.addEventListener('click', playAudio);
+    document.addEventListener('touchstart', playAudio);
+});
